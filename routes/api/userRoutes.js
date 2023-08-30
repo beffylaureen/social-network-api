45a1f3a193616ba7881b5d5956ef02379fb6f8f2
B Laureen <beffylaureen@me.com>
2023-08-29 20:46:12 -0500

    user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUser).post(createUser);
+
+// /api/user/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// /api/user/:userId/friend
+router.route('/:userId/friend').post(addFriend);
+
+// /api/user/:userId/friend/:friendId
+router.route('/:userId/friend/:friendId').delete(removeFriend);
+
+module.exports = router;
